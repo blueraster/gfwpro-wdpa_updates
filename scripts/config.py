b5602a0ffb63df843192019f0e4cfa1d88822810
Dakota <86389347+dakota635@users.noreply.github.com>
2024-08-09 10:47:37 -0500

    Update config.py

--- scripts/config.py
+++ scripts/config.py
@@ -1,8 +1,8 @@
 # For wdpa_quartely_update.py
-CONN_STRING = "DRIVER={ODBC Driver 17 for SQL Server};SERVER=52.45.199.190,65300;DATABASE=gfwpro;UID=GFWProUser;PWD=measure2010!;"
-GDB_LOC = r"D:\data\WDPA_Quarterly_Update\data\2023_Q4\wdpa_2023_q4.gdb"
+CONN_STRING = ""
+GDB_LOC = r""
 
-SAL_STRING = "mssql://GFWProUser:measure2010!@52.45.199.190,65300/gfwpro?driver=ODBC Driver 17 for SQL Server"
+SAL_STRING = ""
 
 # For save_to_database
-INPUT_GDB_PATH = r"C:\Users\dwilburn\Desktop\Projects\GFW Pro\Git\gfwpro-wdpa_updates\data\part-00000-5e5474fb-1179-4e22-aeb7-1e13b628b73b-c000.csv"
+INPUT_GDB_PATH = r""
